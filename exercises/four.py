10fcb052d54088b0c0f5a04e7084ba0cf5cb2070
Calvin Forbes <cforbes@breezeline.com>
2023-05-11 16:57:53 -0400

    rephrase

--- exercises/four.py
+++ exercises/four.py
@@ -1,3 +1,3 @@
 """
-    1. Find the first file that's greater than 500 KB and a decendant of home directory.
+    1. Find the first file that's greater than 500 KB and a decendant of the home directory.
 """
